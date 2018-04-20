d2abddd147a5842bbb52fdba1b35b88ff671130d
Jaydeep Solanki <jaydp17@gmail.com>
2018-04-21 03:16:38 +0530

    create hello world server using restify

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const restify = require('restify');
+
+const server = restify.createServer({
+  name: 'mongodb-on-k8s',
+  version: '1.0.0',
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.get('/', (req, res, next) => {
+  res.send({ hello: 'world' });
+  return next();
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('%s listening at %s', server.name, server.url);
+});
